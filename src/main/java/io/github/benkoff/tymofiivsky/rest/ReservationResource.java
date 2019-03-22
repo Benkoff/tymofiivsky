a493ffd0a3b466c757050ff4870bc4522da1d130
ben <2208Github2202>
2019-03-22 04:30:29 +0200

    Add processing for response mappings

--- src/main/java/io/github/benkoff/tymofiivsky/rest/ReservationResource.java
+++ src/main/java/io/github/benkoff/tymofiivsky/rest/ReservationResource.java
@@ -1,10 +1,13 @@
 package io.github.benkoff.tymofiivsky.rest;
 
+import io.github.benkoff.tymofiivsky.model.request.ReservationRequest;
 import io.github.benkoff.tymofiivsky.model.response.ReservationResponse;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,15 +18,42 @@ import java.time.LocalDate;
 @RestController
 @RequestMapping(ResourseConstants.ROOM_RESERVATION_V1)
 public class ReservationResource {
+
     @RequestMapping(path = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<ReservationResponse> getAvailableRooms(
             @RequestParam(value = "checkin")
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-            LocalDate checkin,
+                    LocalDate checkin,
             @RequestParam(value = "checkout")
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-            LocalDate checkout) {
+                    LocalDate checkout) {
 
         return new ResponseEntity<>(new ReservationResponse(), HttpStatus.OK);
+    }
+
+    @RequestMapping(
+            path = "",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<ReservationResponse> createReservation(@RequestBody ReservationRequest reservationRequest) {
+
+        return new ResponseEntity<>(new ReservationResponse(), HttpStatus.CREATED);
+    }
+
+    @RequestMapping(
+            path = "",
+            method = RequestMethod.PUT,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<ReservationResponse> updateReservation(@RequestBody ReservationRequest reservationRequest) {
+
+        return new ResponseEntity<>(new ReservationResponse(), HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/{reservationId}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteReservation(@PathVariable long reservationId) {
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
