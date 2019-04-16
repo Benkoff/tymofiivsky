5a052e8b1d4b9320600cc6ba0eb1fad8cb3b41f3
ben <2208Github2202>
2019-04-16 18:15:13 +0300

    Add rooms reservation in front

--- src/main/webapp/angular6/src/app/app.component.ts
+++ src/main/webapp/angular6/src/app/app.component.ts
@@ -10,16 +10,23 @@ export class AppComponent implements OnInit{
 
   public submitted: boolean;
   roomSearch: FormGroup;
+  rooms: Room[];
 
   ngOnInit(): void {
     this.roomSearch = new FormGroup({
       checkin: new FormControl(''),
       checkout: new FormControl('')
     });
+
+    this.rooms = ROOMS;
   }
 
   onSubmit({value, valid}: {value:Roomsearch, valid:boolean}) {
     console.log(value);
+  }
+
+  reserveRoom(value:string) {
+    console.log("Room for reservation id:" + value);
   }
 }
 
@@ -27,3 +34,31 @@ export interface Roomsearch {
   checkin:string;
   checkout:string;
 }
+
+export interface Room {
+  id:string;
+  roomNumber:string;
+  price:string;
+  links:string;
+}
+
+var ROOMS: Room[] = [
+  {
+    "id": "100101",
+    "roomNumber": "101",
+    "price": "30",
+    "links": ""
+  },
+  {
+    "id": "100102",
+    "roomNumber": "102",
+    "price": "35",
+    "links": ""
+  },
+  {
+    "id": "100103",
+    "roomNumber": "201",
+    "price": "20",
+    "links": ""
+  }
+];
